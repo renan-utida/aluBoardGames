fd273121cf5b28ab935017786dd7718f338777ba
Renan <renandiutida@gmail.com>
2026-02-03 02:09:09 -0300

    Obtendo o jogo clicado

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+
+
+
+}
